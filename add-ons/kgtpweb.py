94f3967e58b155e4a031ed4a363d4b03eb983d0d
Hui Zhu <teawater@gmail.com>
2014-01-07 15:17:39 +0800

    Fix base kgtpweb.py

--- add-ons/kgtpweb.py
+++ add-ons/kgtpweb.py
@@ -9,6 +9,15 @@ from BaseHTTPServer import HTTPServer
 import urlparse
 import cPickle
 import os
+import re
+import inspect
+
+class config:
+	def __init__(self, value = "", web_set = True,
+		     web_introduce = ""):
+		self.value = value
+		self.web_set = web_set
+		self.web_introduce = web_introduce
 
 class handler(BaseHTTPRequestHandler):
 	def do_GET(self):
@@ -16,10 +25,10 @@ class handler(BaseHTTPRequestHandler):
 
 class web(HTTPServer):
 	orig_config = {
-			'GDB' :		("", 'gdb'),
-			'vmlinux' :	('''<a
+			'GDB' :		config('gdb'),
+			'vmlinux' :	config("", True, '''<a
 href="https://code.google.com/p/kgtp/wiki/HOWTO#Where_is_the_current_Linux_kernel_debug_image"
-target="_blank">introduce</a>''', ''),
+target="_blank">introduce</a>'''),
 		      }
 	config = orig_config
 
@@ -43,12 +52,11 @@ target="_blank">introduce</a>''', ''),
 				self.config[name] = self.orig_config[name]
 
 	def save_config(self):
-		return
 		f = file(self.config_file, 'w')
 		cPickle.dump(self.config, f)
 		f.close()
 
-	def output_msg(self, one_handler, msg):
+	def output_msg(self, one_handler, msg, show_return = True, other_html = ""):
 		one_handler.send_response(200)
 		one_handler.send_header("Content-type", "text/html")
 		one_handler.end_headers()
@@ -58,59 +66,96 @@ target="_blank">introduce</a>''', ''),
 				<body>
 		<div style="text-align: center;">''')
 		one_handler.wfile.write(msg)
-		one_handler.wfile.write('''<br><a href="javascript:" onClick="javascript :window.history.back(-1);">return</a>
-				</div></body></html>''')
+		if show_return:
+			one_handler.wfile.write('''<br><a href="javascript:" onClick="javascript :window.history.back(-1);">return</a>''')
+		one_handler.wfile.write(other_html)
+		one_handler.wfile.write("</div></body></html>")
 
-	def check_gdb(self, gdb):
-		if len(gdb) == 0 or gdb.find(" ") >= 0:
-			return "Format of GDB is not right."
-		f = os.popen(gdb + " -v")
-		return f.read()
-		f.close()
-		return ""
-	def do_setup_set(self, one_handler, query):
-		ret = self.check_gdb(query["GDB"])
-		if ret != "":
-			self.output_msg(one_handler, ret)
-	
-	def do_setup(self, one_handler):
+	def web_config(self, one_handler, config):
 		query = urlparse.parse_qs(one_handler.parsed_path.query,
 					  keep_blank_values=True)
 
 		if len(query) > 0:
 			#Check if all config got return
-			for name in self.config:
-				if name not in query:
+			for name in config:
+				if config[name].web_set and (name not in query):
 					self.output_msg(one_handler,
 							name+" is not send")
-					return
-			self.do_setup_set(one_handler, query)
+					query = {}
 		else:
 			one_handler.send_response(200)
 			one_handler.send_header("Content-type", "text/html")
 			one_handler.end_headers()
 			one_handler.wfile.write('''<html><head>
-				<title>KGTP setup</title>
+				<title>KGTP</title>
 				</head>
 				<body>
 				<form method="get" action="/">
 				<table style="margin-left: auto; margin-right: auto;" border="1" cellpadding="2" cellspacing="0"><tbody>
 				''')
-			for name in self.config:
+			for name in config:
+				if not config[name].web_set:
+					continue
 				one_handler.wfile.write("<tr><td>"+name)
-				if len(self.config[name][0]) > 0:
-					one_handler.wfile.write("("+self.config[name][0]+")")
+				if len(config[name].web_introduce) > 0:
+					one_handler.wfile.write("("+config[name].web_introduce+")")
 				one_handler.wfile.write("</td>")
-				one_handler.wfile.write("<td><input name=\""+name+"\" value=\""+self.config[name][1]+"\"></td>")
+				one_handler.wfile.write("<td><input name=\""+name+"\" value=\""+config[name].value+"\"></td>")
 				one_handler.wfile.write("</tr>\n")
 			one_handler.wfile.write('''<tr>
 			<td style="text-align: center;" colspan="2">
 			<button>OK</button>
 			<button type="reset">RESET</button>
 			</td></tr></tbody></table></form></body></html>''')
+		return query
+
+	def query_to_config(self, config, query):
+		for name in config:
+			if config[name].web_set:
+				config[name].value = query[name][0]
+
+	def check_gdb(self, gdb, version = 7.6):
+		if len(gdb) == 0 or gdb.find(" ") >= 0:
+			return "Format of GDB is not right."
+		try:
+			f = os.popen(gdb + " -v")
+			v = f.readline()
+			f.close()
+		except:
+			return "Cannot exec GDB."
+		if not re.match('^GNU gdb (.+) \d+\.\d+\S+$', v):
+			return "Cannot get version of GDB."
+		v = float(re.search('\d+\.\d+', v).group())
+		if v < version:
+			return '''This GDB is too old.  Please goto <a
+href="https://gdbt.googlecode.com"
+target="_blank">this link</a> get a new version.'''
+		return ""
+
+	def web_kgtp_config(self, one_handler):
+		query = self.web_config(one_handler, self.config)
+		if len(query) <= 0:
+			return
+		#Check GDB.
+		ret = self.check_gdb(query["GDB"][0])
+		if ret != "":
+			self.output_msg(one_handler, ret)
+			return
+		#Check vmlinux.
+		if (query["vmlinux"][0] != "") and (not os.path.exists(query["vmlinux"][0])):
+			self.output_msg(one_handler, "Vmlinux is not exist.")
+			return
+		#Save to config
+		self.query_to_config(self.config, query)
+		self.save_config()
+		#Exec GDB
+		#os.execlp(self.config["GDB"].value,
+			  #self.config["vmlinux"].value,
+			  #"-ex", "source "+os.path.abspath(inspect.getfile(inspect.currentframe())))
+		self.output_msg(one_handler, "Set success.")
 
 	page_list = {
-			"/" : 	do_setup,
+			"/" : 	web_kgtp_config,
 		    }
 
 	def do_GET(self, one_handler):
