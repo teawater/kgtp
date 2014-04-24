98e46683e0540b1a7d0eebb6c764e2a048cf4637
Hui Zhu <teawater@gmail.com>
2014-04-24 09:49:57 +0800

    Remove a file that doesn't need

--- kgtp_bak.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import sys, inspect, os
-sys.path.append(os.path.abspath(os.path.dirname(inspect.getfile(inspect.currentframe()))))
-import kgtp_setup
-
-kgtp_setup.main(sys.argv)
