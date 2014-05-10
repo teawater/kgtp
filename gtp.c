f0219e5f7bc24ff056a0678ee78aa3e7e9af23c6
Hui Zhu <teawater@gmail.com>
2014-05-10 17:41:02 +0800

    Update version to 20140510.

--- gtp.c
+++ gtp.c
@@ -20,7 +20,7 @@
  */
 
 /* If "* 10" means that this is not a release version.  */
-#define GTP_VERSION			(20131220 * 10)
+#define GTP_VERSION			(20140510)
 
 #include <linux/version.h>
 #ifndef RHEL_RELEASE_VERSION
