e99379cc574b91d2afd26c10b7ebd33989c80e82
hustcalm <licalmer@gmail.com>
2014-08-25 16:04:42 +0800

    remove the scripts from main KGTP source code

--- start_gdb.sh
+++
@@ -1,1 +0,0 @@
-sudo gdb /usr/lib/debug/lib/modules/3.14.8-200.fc20.x86_64/vmlinux -ex 'target remote /sys/kernel/debug/gtp' -ex 'set remote trace-buffer-size on'
--- startover_gtp.sh
+++
@@ -1,4 +0,0 @@
-sudo rmmod gtp
-make clean
-make D=1
-sudo insmod gtp.ko
