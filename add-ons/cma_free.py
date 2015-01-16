22173edc2cdfcddbf76a40082d52d73cb9d6a459
Hui Zhu <teawater@gmail.com>
2015-01-16 20:31:39 +0800

    Add a script to show the cma_nr_free and NR_FREE_CMA_PAGES

---
+++ add-ons/cma_free.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import gdb
+
+MAX_ORDER = 11
+NR_FREE_CMA_PAGES = int(gdb.parse_and_eval("NR_FREE_CMA_PAGES"))
+
+def print_zone(num):
+	print str(gdb.parse_and_eval("contig_page_data->node_zones[" + str(num) + "]->name"))
+	print "NR_FREE_CMA_PAGES = " + str(gdb.parse_and_eval("contig_page_data->node_zones[" + str(num) + "]->vm_stat[" + str(NR_FREE_CMA_PAGES) + "]"))
+	count = 0
+	for i in range(0, MAX_ORDER):
+		page = int(gdb.parse_and_eval("contig_page_data->node_zones[" + str(num) + "]->free_area[" + str(i) + "].cma_nr_free"))
+		print i, page, page << i
+		count += page << i
+	print count
+
+print_zone(0)
+print_zone(1)
