14dc4fcc4172c0a3a982d3b0ca26484cdd194e18
Hui Zhu <teawater@gmail.com>
2015-01-26 15:38:50 +0800

    Add nr_free show to cma_free.py

--- add-ons/cma_free.py
+++ add-ons/cma_free.py
@@ -4,16 +4,28 @@ import gdb
 
 MAX_ORDER = 11
 NR_FREE_CMA_PAGES = int(gdb.parse_and_eval("NR_FREE_CMA_PAGES"))
+NR_FREE_PAGES = int(gdb.parse_and_eval("NR_FREE_PAGES"))
 
 def print_zone(num):
 	print str(gdb.parse_and_eval("contig_page_data->node_zones[" + str(num) + "]->name"))
 	print "NR_FREE_CMA_PAGES = " + str(gdb.parse_and_eval("contig_page_data->node_zones[" + str(num) + "]->vm_stat[" + str(NR_FREE_CMA_PAGES) + "]"))
 	count = 0
+	print "cma_nr_free"
 	for i in range(0, MAX_ORDER):
 		page = int(gdb.parse_and_eval("contig_page_data->node_zones[" + str(num) + "]->free_area[" + str(i) + "].cma_nr_free"))
 		print i, page, page << i
 		count += page << i
 	print count
+	print ""
+	print "NR_FREE_PAGES = " + str(gdb.parse_and_eval("contig_page_data->node_zones[" + str(num) + "]->vm_stat[" + str(NR_FREE_PAGES) + "]"))
+	count = 0
+	print "nr_free"
+	for i in range(0, MAX_ORDER):
+		page = int(gdb.parse_and_eval("contig_page_data->node_zones[" + str(num) + "]->free_area[" + str(i) + "].nr_free"))
+		print i, page, page << i
+		count += page << i
+	print count
+	print ""
 
 print_zone(0)
 print_zone(1)
